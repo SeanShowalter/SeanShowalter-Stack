9fa6fb036649c81ba59232e40be1bf92dffd5207
Sean Showalter <sean@showalter.design>
2023-02-26 04:43:18 +0000

    Tests passed.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,7 @@ public class Application {
     /**
      * This class contains a main method that allows you to manually test the Stack lab.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
-     * All implementation code should be written in the Lab class.
+     * All implementation code should be written in the Lab class. 
      *
      * The Lab methods should perform Stack operations.
      */
